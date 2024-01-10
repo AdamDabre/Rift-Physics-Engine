6eeceb1bb812ea321439647d27d9f222414b762f
Adam Dabre <adamdabre23@gmail.com>
2024-01-10 15:55:01 +0800

    Initial Project Push
    Added the demo project and core project

---
+++ Demo/Rift Physics Demo/Rift Physics Demo.cpp
@@ -0,0 +1,12 @@
+#include "../../Rift Physics Engine/RiftPhysics.h"
+
+#include <iostream>
+
+int main()
+{
+    
+    RiftPhysics::Vec3 myVec(2, 0, 0);
+
+    std::cout << myVec.x << std::endl;
+}
+
---
+++ Rift Physics Engine/src/Math/Math.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp""
+
+namespace RiftPhysics 
+{
+		// Defines a 3D vector
+	using Vec3 = glm::vec3;
+		// Defines a 2D vector
+	using Vec2 = glm::vec2;
+}
